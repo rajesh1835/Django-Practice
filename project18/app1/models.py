af5f21252e5266ec83e9369e80042969ce3f32b9
Tarigonda Rajesh <rajeshtarigonda@gmail.com>
2026-09-07 20:39:29 +0530

    Added new colums to the Existing tables

--- project18/app1/models.py
+++ project18/app1/models.py
@@ -9,9 +9,10 @@ class Webpage(models.Model):
     topic_name = models.ForeignKey(Topic, on_delete=models.CASCADE)
     name = models.CharField()
     url = models.URLField()
+    email = models.EmailField(null=True, blank=True)
+    mobile = models.CharField(max_length=10)
 
 class AccessRecord(models.Model):
     name = models.ForeignKey(Webpage, on_delete=models.CASCADE)
     author = models.CharField()
     date = models.DateField()
-
